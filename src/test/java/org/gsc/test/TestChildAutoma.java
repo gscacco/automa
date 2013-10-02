3a451bdbfaaf56e833a1f727ed46895de523af14
Gianluca Scacco <gianluca.scacco@next.it>
2013-10-02 23:16:07 +0200

    Refactoring

--- src/test/java/org/gsc/test/TestChildAutoma.java
+++ src/test/java/org/gsc/test/TestChildAutoma.java
@@ -3,9 +3,14 @@ package org.gsc.test;
 import org.gsc.automa.Automa;
 import org.gsc.test.utils.AutomaTestCase;
 import org.gsc.test.utils.SpyAction;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestChildAutoma extends AutomaTestCase {
+
+    private Automa automa;
+    private SpyAction childAction;
+    private SpyAction parentAction;
 
     enum State {
         START,
@@ -17,37 +22,31 @@ public class TestChildAutoma extends AutomaTestCase {
         RUNNING
     }
 
-    /**
-     * This test needs to be implemented.
-     */
-    @Test
-    public void shouldExecuteChildAutomaAction() {
-        // setup
-        Automa automa = new Automa(State.START);
-        final SpyAction action = new SpyAction();
-        // exercise
-        automa.addChildAutoma(
-                State.START,
-                new Automa(StartSubState.LOADING) {{
-                    from(StartSubState.LOADING).goTo(StartSubState.RUNNING).when(FakeEvent.EVENT_1).andDo(action);
-                }});
-        automa.signalEvent(FakeEvent.EVENT_1);
-        // verify
-        action.assertExecuted();
-    }
-
-    @Test
-    public void shouldGivePriorityToParent() {
-        // setup
-        Automa automa = new Automa(State.START);
-        final SpyAction childAction = new SpyAction();
-        SpyAction parentAction = new SpyAction();
-
+    @Before
+    public void before() {
+        automa = new Automa(State.START);
+        childAction = new SpyAction();
+        parentAction = new SpyAction();
         automa.addChildAutoma(
                 State.START,
                 new Automa(StartSubState.LOADING) {{
                     from(StartSubState.LOADING).goTo(StartSubState.RUNNING).when(FakeEvent.EVENT_1).andDo(childAction);
                 }});
+    }
+
+
+    @Test
+    public void shouldExecuteChildAutomaAction() {
+        // setup
+        // exercise
+        automa.signalEvent(FakeEvent.EVENT_1);
+        // verify
+        childAction.assertExecuted();
+    }
+
+    @Test
+    public void shouldGivePriorityToParent() {
+        // setup
         automa.from(State.START).goTo(State.END).when(FakeEvent.EVENT_1).andDo(parentAction);
         // exercise
         automa.signalEvent(FakeEvent.EVENT_1);
@@ -59,15 +58,6 @@ public class TestChildAutoma extends AutomaTestCase {
     @Test
     public void shouldGivePriorityToParentOnStay() {
         // setup
-        Automa automa = new Automa(State.START);
-        final SpyAction childAction = new SpyAction();
-        SpyAction parentAction = new SpyAction();
-
-        automa.addChildAutoma(
-                State.START,
-                new Automa(StartSubState.LOADING) {{
-                    from(StartSubState.LOADING).goTo(StartSubState.RUNNING).when(FakeEvent.EVENT_1).andDo(childAction);
-                }});
         automa.from(State.START).stay().when(FakeEvent.EVENT_1).andDo(parentAction);
         // exercise
         automa.signalEvent(FakeEvent.EVENT_1);
